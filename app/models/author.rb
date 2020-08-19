82fed09802cbf695e945093bc089a831288528a6
Shawn Kearney <shawnpk29@me.com>
2020-08-19 08:32:15 -0600

    Add association from author to posts

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,4 +1,6 @@
 class Author < ApplicationRecord
+  has_many :posts
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
