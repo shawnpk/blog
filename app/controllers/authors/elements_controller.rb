d3ba62f61d6b7afd1da94bc4559c31f099ed1415
Shawn Kearney <shawnpk29@me.com>
2020-08-22 15:22:37 -0500

    Remove unnecessary actions, update create action

--- app/controllers/authors/elements_controller.rb
+++ app/controllers/authors/elements_controller.rb
@@ -1,35 +1,19 @@
 module Authors
   class ElementsController < AuthorsController
     before_action :set_post
-    before_action :set_element, only: [:show, :edit, :update, :destroy]
-
-    # GET /elements
-    def index
-      @elements = Element.all
-    end
-
-    # GET /elements/1
-    def show
-    end
-
-    # GET /elements/new
-    def new
-      @element = Element.new
-    end
-
-    # GET /elements/1/edit
-    def edit
-    end
+    before_action :set_element, only: [:update, :destroy]
 
     # POST /elements
     def create
-      @element = Element.new(element_params)
+      @element = @post.elements.build(element_params)
 
       if @element.save
-        redirect_to @element, notice: 'Element was successfully created.'
+        notice = nil
       else
-        render :new
+        notice = @element.errors.full_messages.join('. ') << '.'
       end
+
+      redirect_to edit_post_path(@post), notice: notice
     end
 
     # PATCH/PUT /elements/1
@@ -59,7 +43,7 @@ module Authors
 
       # Only allow a trusted parameter "white list" through.
       def element_params
-        params.require(:element).permit(:element_type, :content, :post_id, :position)
+        params.require(:element).permit(:element_type, :content)
       end
   end
 end
