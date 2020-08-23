72766c7e0ddd35ea3768f49dad15747ec8275b7e
Shawn Kearney <shawnpk29@me.com>
2020-08-23 11:06:05 -0600

    Add ability to create images

--- app/controllers/authors/elements_controller.rb
+++ app/controllers/authors/elements_controller.rb
@@ -40,7 +40,7 @@ module Authors
 
       # Only allow a trusted parameter "white list" through.
       def element_params
-        params.require(:element).permit(:element_type, :content)
+        params.require(:element).permit(:element_type, :content, :image)
       end
   end
 end
--- app/models/element.rb
+++ app/models/element.rb
@@ -1,6 +1,15 @@
 class Element < ApplicationRecord
-  belongs_to :post
-  has_rich_text :content
+  belongs_to       :post
+  has_rich_text    :content
+  has_one_attached :image
 
   validates :element_type, inclusion: { in: %w[paragraph image video-embed]}
+
+  def paragraph?
+    element_type == 'paragraph'
+  end
+
+  def image?
+    element_type == 'image'
+  end
 end
