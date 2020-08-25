196b6ff752d5651a18e9a305c5aad237130b9af7
Shawn Kearney <shawnpk29@me.com>
2020-08-24 19:37:55 -0600

    Install optimism gem

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,7 @@ class Post < ApplicationRecord
   belongs_to :author
   has_many :elements
   has_one_attached :header_image
+
+  validates :title, presence: true
+  validates :description, presence: true, length: { within: 50..250 }
 end
