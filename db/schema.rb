60d4b07a7beafae3f5e96be227a7ddd1f1b9fcf4
Shawn Kearney <shawnpk29@me.com>
2020-08-25 06:30:51 -0600

    Install friendly_id

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_25_122928) do
+ActiveRecord::Schema.define(version: 2020_08_25_123029) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -88,7 +88,9 @@ ActiveRecord::Schema.define(version: 2020_08_25_122928) do
     t.bigint "author_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "slug"
     t.index ["author_id"], name: "index_posts_on_author_id"
+    t.index ["slug"], name: "index_posts_on_slug", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
