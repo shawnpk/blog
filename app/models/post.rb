ae0774980bff321be4e5da6c581b87c7c5ed32ba
Shawn Kearney <shawnpk29@me.com>
2020-08-25 12:59:43 -0600

    Add structure and styles to root view

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,6 +10,10 @@ class Post < ApplicationRecord
   validates :title, presence: true
   validates :description, presence: true, length: { within: 50..250 }
 
+  scope :published, -> { where(published: true) }
+
+  scope :most_recently_published, -> { order(published_at: :desc) }
+
   def should_generate_new_friendly_id?
     title_changed?
   end
