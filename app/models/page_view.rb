fd34838627bc0215279a8d198d904231402d3601
Shawn Kearney <shawnpk29@me.com>
2020-08-26 07:40:03 -0600

    Create scopes for various impression calls

--- app/models/page_view.rb
+++ app/models/page_view.rb
@@ -1,9 +1,10 @@
 class PageView < Impression
   # TODO:
-  # 1. Define scope for counts for last 7 days (or custom range)
-  # 2. Define scope for uniq counts for last 7 days (or custom range)
-  # 3. Define scope for page ranks by count
-  # 4. Define scope for page ranks by uniq count
+  # 1. Define scope for page ranks by count
+  # 2. Define scope for page ranks by uniq count
   
-  scope :for_type, -> (type) { where(impressionable_type: type) }
+  scope :for_type,           -> (type) { where(impressionable_type: type) }
+  scope :for_date_range,     -> (start_date, end_date) { where(created_at: start_date.beginning_of_day..end_date.end_of_day) }
+  scope :count_by_date,      -> () { group('date(created_at)').count }
+  scope :uniq_count_by_date, -> () { PageView.select(:ip_address).distinct.group('date(created_at)').count }
 end
